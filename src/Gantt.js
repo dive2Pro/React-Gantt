8819bd5deaa5a15e0831195baaeb57b513c26afd
dive2Pro <toylucklebo@gmail.com>
2018-04-21 23:31:46 +0800

    绘制 usedTime

--- src/Gantt.js
+++ src/Gantt.js
@@ -1,4 +1,6 @@
 import React from "react";
+import moment from "moment";
+
 const GanttContext = React.createContext({});
 
 const YAxis = () => {
@@ -7,7 +9,7 @@ const YAxis = () => {
       {({ data, lineHeight: h, yAxisWidth }) => {
         const startX = 0,
           startY = 0;
-        return data.map(({ name }, i) => {
+        return data.map(({ YAxis: name }, i) => {
           return (
             <React.Fragment key={name}>
               <g>
@@ -36,24 +38,19 @@ const YAxis = () => {
 const HelpRects = () => {
   return (
     <GanttContext.Consumer>
-      {({
-        yAxisWidth: ox,
-        lineHeight: h,
-        proption,
-        xLeft,
-        data,
-        xAxisWidth
-      }) => {
+      {({ lineHeight: h, proption, xLeft, data, xAxisWidth }) => {
         const columns = 48;
         const rows = data.length;
         let rects = [];
         const width = xAxisWidth / columns / proption;
+        const transform = `translate( ${xLeft}, 0)`;
         for (let r = 0; r < rows; r++) {
           let y = h * r;
           for (let c = 0; c < columns; c++) {
-            let x = ox + width * c;
+            let x = width * c;
             rects.push(
               <rect
+                transform={transform}
                 stroke="blue"
                 strokeWidth={0.2}
                 strokeOpacity={0.5}
@@ -74,18 +71,66 @@ const HelpRects = () => {
   );
 };
 
+const Datas = () => {
+  return (
+    <GanttContext.Consumer>
+      {({ data, lineHeight: h, xAxisWidth }) => {
+        const m = moment(data[0].usedTime.startTime).startOf("day");
+        // const s = moment(date[0].usedTime.startTime).startOf('day');
+        console
+          .log
+          // m.get('millisecond'),
+          // m.get('month')
+          ();
+        const initialTime = m.valueOf();
+        const dayMillisedons = 1000 * 3600 * 24;
+
+        return data.map(({ name, usedTime }, i) => {
+          // 计算 x
+          //   1. x 轴 原点是 这一天的 起点时间
+          //   2. deltaTime = startTime - initialTime
+          const { startTime, endTime } = usedTime;
+          const startMilliseconds = moment(startTime).valueOf();
+          const endMilliseconds = moment(endTime).valueOf();
+          const deltaTime = startMilliseconds - initialTime;
+          //   3. 总长度是固定的 xAxisWidth, 所以 x / xAxisWidth = deltaTime / fullDayTime 这里的 x = initialX
+          const widthProption = deltaTime / dayMillisedons;
+          const initialX = widthProption * xAxisWidth;
+
+          //   4. 同样的方式 拿到 initialWidth
+          const initialWidth =
+            (endMilliseconds - startMilliseconds) / dayMillisedons * xAxisWidth;
+          //   5. AWait 组件的终点是 initialX, 宽度是 此任务和上一个任务的 时间间隔
+          //   6
+
+          return (
+            <React.Fragment key={name}>
+              <g>
+                <text y={i * h + 12} fontSize={12} x={0} height={h / 3}>
+                  {name}
+                </text>
+                <rect
+                  x={initialX}
+                  y={i * h + h / 3}
+                  width={initialWidth}
+                  height={h / 3}
+                />
+              </g>
+            </React.Fragment>
+          );
+        });
+      }}
+    </GanttContext.Consumer>
+  );
+};
+
 class XAxis extends React.Component {
   render() {
     return (
-      <GanttContext.Consumer>
-        {({ yAxisWidth: ox, data, lineHeight: h }) => {
-          return (
-            <React.Fragment>
-              <HelpRects />
-            </React.Fragment>
-          );
-        }}
-      </GanttContext.Consumer>
+      <React.Fragment>
+        <HelpRects />
+        <Datas />
+      </React.Fragment>
     );
   }
 }
@@ -95,8 +140,12 @@ class Chart extends React.PureComponent {
     const { yAxisWidth, xAxisWidth, xAxisHeight } = this.props;
     return (
       <svg width={yAxisWidth + xAxisWidth} height={xAxisHeight}>
-        <YAxis />
-        <XAxis />
+        <svg width={yAxisWidth}>
+          <YAxis />
+        </svg>
+        <svg x={yAxisWidth}>
+          <XAxis />
+        </svg>
       </svg>
     );
   }
@@ -115,7 +164,7 @@ export default class ReactGantt extends React.PureComponent {
   };
   state = {
     proption: 1,
-    xLeft: 0
+    xLeft: -1 * 0
   };
   render() {
     const { xAxisHeight, ...rest } = this.props;
